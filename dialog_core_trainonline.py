d1beb8b177f036df259c591dcb49de85e66ec0a0
Ahmed Shehata Alex <ahmed.shehata@rutgers.edu>
2018-12-11 01:55:59 +0200

    Add files via upload

---
+++ dialog_core_trainonline.py
@@ -0,0 +1,40 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import logging
+
+from rasa_core.agent import Agent
+from rasa_core.policies.keras_policy import KerasPolicy
+from rasa_core.policies.memoization import MemoizationPolicy
+from rasa_core.interpreter import RasaNLUInterpreter
+from rasa_core.train import interactive
+from rasa_core.utils import EndpointConfig
+
+logger = logging.getLogger(__name__)
+
+
+def run_laliga_online(interpreter,
+                          domain_file="./LaLiga_bot/Laliga_domain.yml",
+                          training_data_file='./LaLiga_bot/data/stories.md'):
+    action_endpoint = EndpointConfig(url="http://localhost:5055/webhook")						  
+    '''
+    train from scratch
+    agent = Agent(domain_file,
+                  policies=[MemoizationPolicy(max_history=2), KerasPolicy(max_history=3, epochs=3, batch_size=50)],
+                  interpreter=interpreter,
+				  action_endpoint=action_endpoint)
+    				  
+    data = agent.load_data(training_data_file)			   
+    agent.train(data)
+    ''' 
+    agent = Agent.load(r'.\LaLiga_bot\models\dialogue', interpreter=interpreter, action_endpoint=action_endpoint)
+    interactive.run_interactive_learning(agent, training_data_file)
+    return agent
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level="INFO")
+    nlu_interpreter = RasaNLUInterpreter(r'.\LaLiga_bot\models\laligamodelnlu\nlumodel')
+    run_laliga_online(nlu_interpreter)
